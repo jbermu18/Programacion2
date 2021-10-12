482e2254eb2bf28ef5653920bf17cdd343bcc1b0
Javi Bermudez <jbermudezfernandez@danielcastelao.org>
2021-10-12 18:38:05 +0200

    Agregado

--- src/com/programacion/Main.java
+++ src/com/programacion/Main.java
@@ -3,6 +3,6 @@ package com.programacion;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hola Mundo");
+	System.out.println("Hola Mundo, como estais");
     }
 }
